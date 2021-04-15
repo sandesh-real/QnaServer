1f13472f2b31b35e079020e7f50fe75837b9adb6
sandesh sharma <sharmamasandesh@gmail.com>
2021-04-16 01:53:20 +0545

    votes

--- src/db/mongoosedb.js
+++ src/db/mongoosedb.js
@@ -4,5 +4,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/QnA',{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology:true,
-    useFindAndModify:true
+    useFindAndModify:false    
 })
--- src/routers/votes.js
+++ src/routers/votes.js
@@ -11,22 +11,23 @@ router.post('/voteChange',auth,async (req,res)=>{
 
     try{
     const votesQuestion=await Votes.find({question_id:req.body.q_id});
+   
     const questionTest=await Question.find({user_id:req.user._id,_id:req.body.q_id})
     let votesChanges=votesQuestion[0].counts;
     let changeQuestion='';
     let userIdCollection='';
-  
-         userIdCollection= votesQuestion[0].useCollection.split(',');
+    userIdCollection= votesQuestion[0].useCollection.split(',');
     
   
     if(questionTest.length===0)
     {
-       if(userIdCollection.length>0){
+      
+       if(userIdCollection.length>1){
 
       
         if(req.body.val==='inc' &&  !userIdCollection.includes(req.user._id.toString() + ' inc'))
         {
-            console.log("inc 1")
+            
            userIdCollection= userIdCollection.filter((item)=>{
                 return item!==req.user._id.toString() + ' dec'
             })
@@ -34,7 +35,7 @@ router.post('/voteChange',auth,async (req,res)=>{
             votesChanges=parseInt(votesQuestion[0].counts)+1
         }
         else if(req.body.val==='dec' &&  !userIdCollection.includes(req.user._id.toString() + ' dec')) {
-            console.log("dec 1")
+           
             userIdCollection.push(`${req.user._id.toString()} dec`)
             userIdCollection= userIdCollection.filter((item)=>{
                 return item!==req.user._id.toString() + ' inc'
@@ -45,19 +46,19 @@ router.post('/voteChange',auth,async (req,res)=>{
     else{
         if(req.body.val==='inc')
         {
-            console.log("inc 0")
+           
             userIdCollection.push(`${req.user._id.toString()} inc`)
             votesChanges=parseInt(votesQuestion[0].counts)+1
         }
         else if(req.body.val==='dec'){
-            console.log("dec 0")
+          
             userIdCollection.push(`${req.user._id.toString()} dec`)
         
             votesChanges=parseInt(votesQuestion[0].counts)-1
         }
     }
-         await Votes.updateOne({counts:votesChanges});
-         changeQuestion= await Votes.updateOne({useCollection:userIdCollection.toString()})
+         await Votes.findOneAndUpdate({question_id:req.body.q_id},{counts:votesChanges});
+         changeQuestion= await Votes.findOneAndUpdate({question_id:req.body.q_id},{useCollection:userIdCollection.toString()})
 
          res.json({votes:votesChanges})
 
@@ -77,7 +78,8 @@ router.get("/getVotes",auth,async (req,res)=>{
 try{
     
 let votes=await Votes.find({question_id:req.query.q_id});
-res.json({votes:votes[0].counts})
+console.log(votes);
+res.json({votes:votes[0].counts,userVote:votes[0].useCollection})
 }
 catch(e)
 {
